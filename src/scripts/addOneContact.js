6803847112df036acd6cf5118da0c9b62a750c11
EldarMemetov <eldarm9347@gmail.com>
2024-08-21 20:11:07 +0200

    update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -6,18 +6,20 @@ export const addOneContact = async (num) => {
   try {
     let data = await fs.readFile(PATH_DB, 'utf-8');
     let contacts = JSON.parse(data);
-    const newContacts = [];
-    for (let i = 0; i < num; i += 1) {
-      newContacts.push(createFakeContact());
-    }
+
+    const newContacts = Array(num)
+      .fill()
+      .map(() => createFakeContact());
+
     contacts = [...contacts, ...newContacts];
 
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
-    console.log(`Успішно доданий   ${num} контакт`);
+    console.log(`Успішно доданий ${num} контакт.`);
   } catch (error) {
     console.error('Помилка:', error);
   }
 };
+
 const numContacts = 1;
 await addOneContact(numContacts);
 
